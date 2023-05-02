8ece1e2759d66994e68546207831c51571c6338c
areyes-bsale <areyes@imaginex.cl>
2023-05-02 09:04:55 -0400

    added some exercises

---
+++ excercises.go
@@ -0,0 +1,44 @@
+package main
+
+// Factorial .
+//
+// Implemente una función que calcule el factorial de un número
+// El factorial de un númenro se define como la multiplicación sucesiva de los antecesores de un número hasta el número dado.
+// Ejemplo: f!(5) =  1 * 2 * 3 * 4 * 5 = 120
+//
+// Esta implementación debe hacerse con un loop
+func Factorial(n int64) int64 {
+	panic("you should implement this function")
+}
+
+// FactorialRecursivo .
+//
+// El mismo problema del ejercicio anterior.
+// Esta implementación debe hacerse aplicando recursión
+func FactorialRecursivo(n int64) int64 {
+	panic("you should implement this function")
+}
+
+// FactorialWithError .
+//
+// Extienda la función Factorial para que además del resultado devuelva el posible error de la operación.
+//
+// Si el argumento es menor a 0, devuelva 0 y un error indicando que solo se soportan números positivos.
+// Si el argumento es mayor a 10 devuelva 0 y un error indicando que solo se soporta el calculo del factorial hasta el número 10
+//
+// Esta implementación debe hacerse con un loop
+func FactorialWithError(n int64) (int64, error) {
+	panic("you should implement this function")
+}
+
+// FactorialRecursiveWithError .
+//
+// Extienda la función Factorial para que además del resultado devuelva el posible error de la operación.
+//
+// Si el argumento es menor a 0, devuelva 0 y un error indicando que solo se soportan números positivos.
+// Si el argumento es mayor a 10 devuelva 0 y un error indicando que solo se soporta el calculo del factorial hasta el número 10
+//
+// Esta implementación debe hacerse con un loop
+func FactorialRecursiveWithError(n int64) (int64, error) {
+	panic("you should implement this function")
+}
---
+++ excercises_test.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFactorial(t *testing.T) {
+	type args struct {
+		n int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "Factorial de 5",
+			args: struct{ n int64 }{n: 5},
+			want: 120,
+		},
+		{
+			name: "Factorial de 8",
+			args: struct{ n int64 }{n: 8},
+			want: 40320,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Factorial(tt.args.n); got != tt.want {
+				t.Errorf("Factorial() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFactorialRecursivo(t *testing.T) {
+	type args struct {
+		n int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "Factorial de 5",
+			args: struct{ n int64 }{n: 8},
+			want: 40320,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FactorialRecursivo(tt.args.n); got != tt.want {
+				t.Errorf("FactorialRecursivo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFactorialWithError(t *testing.T) {
+	type args struct {
+		n int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int64
+		wantErr bool
+	}{
+		{
+			name:    "factorial de 5",
+			args:    args{5},
+			want:    120,
+			wantErr: false,
+		},
+		{
+			name:    "factorial de 8",
+			args:    args{8},
+			want:    40320,
+			wantErr: false,
+		},
+		{
+			name:    "factorial de -5",
+			args:    args{-5},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name:    "factorial de 16",
+			args:    args{16},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FactorialWithError(tt.args.n)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FactorialWithError() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FactorialWithError() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFactorialRecursiveWithError(t *testing.T) {
+	type args struct {
+		n int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int64
+		wantErr bool
+	}{
+		{
+			name:    "factorial de 5",
+			args:    args{5},
+			want:    120,
+			wantErr: false,
+		},
+		{
+			name:    "factorial de 8",
+			args:    args{8},
+			want:    40320,
+			wantErr: false,
+		},
+		{
+			name:    "factorial de -5",
+			args:    args{-5},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name:    "factorial de 16",
+			args:    args{16},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FactorialRecursiveWithError(tt.args.n)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FactorialRecursiveWithError() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FactorialRecursiveWithError() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
